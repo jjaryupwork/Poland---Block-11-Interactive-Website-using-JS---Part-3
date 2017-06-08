f8750370b6175e41a1a584d20c2e22d0c87335f5
jjaryupwork <jjaryupwork@gmail.com>
2017-06-08 21:00:53 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,40 @@
-var eventListener = function (){}
-var main = function () {
-    eventListener();
-    if ("geolocation" in navigator) {
-       setInterval(getWeather, 10000);
-        getWeather();
-    }
-    else {
-      alert('aaa');
-        $(".error").html('<p>' + error + '</p>');
-    }
-}
-var getWeather = function () {
+function getWeather() {
+  if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(function (position) {
-        loadWeather(position.coords.latitude + "," + position.coords.longitude);
+      loadWeather(position.coords.latitude + ',' + position.coords.longitude);
     });
+  } else {
+    loadWeather("Warsaw, PL", '');
+  }
 }
-var loadWeather = function (location, units) {
-    $.simpleWeather({
-        location: location,
-        woeid: "",
-        unit: 'c',
-      success: function (weather) {
+
+$(document).ready(function () {
+  setInterval(getWeather, 10000);
+  getWeather();
+});
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'c',
+    success: function (weather) {
       city = weather.city;
       temp = weather.temp + '&deg;';
       wcode = '<img class="weathericon" src="images/' + weather.code + '.svg">';
       wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
       humidity = weather.humidity + '%';
+      
       $(".location").text(city);
       $(".temperature").html(temp);
       $(".climate_bg").html(wcode);
       $(".windspeed").html(wind);
       $(".humidity").text(humidity);
+      
     },
+    
     error: function (error) {
-      alert('aaa');
       $(".error").html('<p>' + error + '</p>');
-  }
-    });
-};
-
-$(document).ready(main);
+    }
+  });
+}
