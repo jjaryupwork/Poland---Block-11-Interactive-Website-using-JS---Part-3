35b46fbae5b1656791f0fb37c7a5ce61ebbd960b
jjaryupwork <jjaryupwork@gmail.com>
2017-06-08 15:19:55 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,53 +1,4 @@
-/*var intervalID = 0;
-var interval = 10000 //default 10 seconds
-var tempUnits = "c"; //default Celsius 
-
-var main = function () {
-    addDOMEventListeners();
-    if ("geolocation" in navigator) {
-        getWeather();
-        intervalID = setInterval(getWeather, interval);
-    }
-    else {
-        $(".error-container > p").text("We apologize but your browser doesn't support geolocation.");
-        $(".error-container").slideDown(400);
-    }
-}
-
-var getWeather = function () {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        loadWeather(position.coords.latitude + "," + position.coords.longitude, tempUnits);
-    });
-}
-
-var loadWeather = function (location, units) {
-    $.simpleWeather({
-        location: location,
-        woeid: "",
-        unit: units,
-        success: function (weather) {
-            $(".location").text(weather.city + ", " + weather.region);
-            $(".temperature").html(weather.temp + "&#176;" + weather.units.temp);
-            $(".climate").html("<img src='./images/" + weather.code + ".svg' alt='weather icon' />");  
-            $(".humidity").text(weather.humidity + "%");
-            $(".windspeed").text(weather.wind.speed + " " + weather.units.speed);
-            var d = new Date();
-            $(".check-time").text("last update: " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
-        },
-
-        error: function (err) {
-            var d = new Date();
-            $(".error-container > p").text(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + " - " + err);
-            $(".error-container").slideDown(400);
-        }
-    });
-};
-*/
 var eventListener = function (){}
-
-
-
-
 var main = function () {
     eventListener();
     if ("geolocation" in navigator) {
@@ -64,7 +15,6 @@ var getWeather = function () {
         loadWeather(position.coords.latitude + "," + position.coords.longitude);
     });
 }
-
 var loadWeather = function (location, units) {
     $.simpleWeather({
         location: location,
@@ -88,8 +38,5 @@ var loadWeather = function (location, units) {
   }
     });
 };
-
-
-
 
 $(document).ready(main);
