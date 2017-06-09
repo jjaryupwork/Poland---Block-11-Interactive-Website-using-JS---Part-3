1c2285ce23c23fa92655111688163786eb477711
jjaryupwork <jjaryupwork@gmail.com>
2017-06-09 19:34:07 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,20 @@ function getWeather() {
   if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(function (position) {
       loadWeather(position.coords.latitude + ',' + position.coords.longitude);
-    });
-  } else {
-    loadWeather("Warsaw, PL", '');
+      function error(error) {
+  alert('msg');}
+      
+
+     }); 
+
   }
 }
+
+
+  // else {
+  //  loadWeather("Warsaw, PL", '');
+ // }
+
 
 $(document).ready(function () {
   setInterval(getWeather, 10000);
