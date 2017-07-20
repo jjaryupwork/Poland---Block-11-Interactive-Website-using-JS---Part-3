1fea85b7a625fe512365da2b069ff578bfd33e71
jjaryupwork <jjaryupwork@gmail.com>
2017-07-20 20:05:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,49 @@
-var geolocationSuccessHandler =   function(position) {
-                loadWeather(position.coords.latitude + ',' + position.coords.longitude);
-};     
+var geolocationSuccessHandler = function(position) {
+    loadWeather(position.coords.latitude + ',' + position.coords.longitude);
+};
 
 var geolocationErrorHandler = function() {
-                alert('You dont allow to use geolocation');
-                loadWeather("Cracow, PL", 'c');
-}     
+    alert('You dont allow to use geolocation');
+    loadWeather("Cracow, PL", 'c');
+}
+
 function getWeather() {
     if ('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(geolocationSuccessHandler,
-            geolocationErrorHandler );
+            geolocationErrorHandler);
     } else {
-       loadWeather("Warsaw, PL", 'c');
+        loadWeather("Warsaw, PL", '');
     }
 }
 
 
-$(document).ready(function () {
-  setInterval(getWeather, 3600000);
-  getWeather();
+$(document).ready(function() {
+    setInterval(getWeather, 3600000);
+    getWeather();
 });
 
 function loadWeather(location, woeid) {
-  $.simpleWeather({
-    location: location,
-    woeid: woeid,
-    unit: 'c',
-    success: function (weather) {
-      city = weather.city;
-      temp = weather.temp + '&deg;';
-      wcode = '<img class="weathericon" src="images/' + weather.code + '.svg">';
-      wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
-      humidity = weather.humidity + '%';
-      
-      $(".location").text(city);
-      $(".temperature").html(temp);
-      $(".climate_bg").html(wcode);
-      $(".windspeed").html(wind);
-      $(".humidity").text(humidity);
-      
-    },
-    
-    error: function (error) {
-      $(".error").html('<p>' + error + '</p>');
-    }
-  });
+    $.simpleWeather({
+        location: location,
+        woeid: woeid,
+        unit: 'c',
+        success: function(weather) {
+            city = weather.city;
+            temp = weather.temp + '&deg;';
+            wcode = '<img class="weathericon" src="images/' + weather.code + '.svg">';
+            wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
+            humidity = weather.humidity + '%';
+
+            $(".location").text(city);
+            $(".temperature").html(temp);
+            $(".climate_bg").html(wcode);
+            $(".windspeed").html(wind);
+            $(".humidity").text(humidity);
+
+        },
+
+        error: function(error) {
+            $(".error").html('<p>' + error + '</p>');
+        }
+    });
 }
